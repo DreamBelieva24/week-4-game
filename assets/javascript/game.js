611cef9330d243557870709ab8050e7766ba0670
Jennifer Campbell <jpagliarulo@Staff-14.local>
2017-12-15 23:18:29 -0500

    almost done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,61 +1,54 @@
-// Show random number
-// Click on crystal, add random amount to player's score.
-// Wins if total score === random score
-// Loses if total score > random score
-// Game restarts
-// Add to wins and losses
-// Random number from 19-120
-// Crystal 1-12
 
-
-var randomNumber = Math.floor((Math.random() * (120 - 19 + 1)) + 19);
-var playerScore = Math.floor((Math.random() * (12 - 1 + 1)) + 1);
-var total = 0;
+var playerScore;
+var randomNumber;
 var wins = 0;
 var losses = 0;
 
-$("#wins").html(wins);
-wins++
-$("#losses").html(losses);
-$("#random").html(randomNumber);
-
-var totalArray = [];
-
-function player(){
-    
-    totalArray.push(playerScore);
-    
-    for (var i = 0; i < totalArray.length; i++) {
-        total += totalArray[i];
-    }
-    
-    $("#score").text(total);
-    randomNumber = parseInt(randomNumber);
-    total = parseInt(total);
-    check();
-}
-
-$(".gem").click(player);
-
-function check(){
-    if (total == randomNumber){
-    wins++;
-    $("#message").html("<h2>You Win!</h2>");
-    reset();
-}
-
-    else if (total > randomNumber){
-    losses++;
-    $("#message").html("<h2>You Lose!</h2>");
-    reset();
-
-    
-}};
+$("#message").click(reset);
 
 
 function reset(){
     total = 0;
+
     $("#score").text("");
+    $("#message").html("");
+    $("#wins").html(wins);
+    $("#losses").html(losses);
+
+    var randomNumber = Math.floor((Math.random() * (120 - 19 + 1)) + 19);
+    $("#random").html(randomNumber);
+
+    $(".gem").click(player);
     
+function player(){
+    var playerScore = Math.floor((Math.random() * (12 - 1 + 1)) + 1);
+
+        total = total + playerScore;
+    
+    
+    $("#score").text(total);
+
+    check();
+}
+
+function check(){
+    if (total == randomNumber){
+    wins++;
+    $("#wins").html(wins);
+    $("#message").html("<h2>You Win!<br><br> Click Here for New Game </h2>");
+    $document.getElementById("footer").innerHTML = '<audio id= "audio"  autoplay src="assets/javascript/sloth.mp3"></audio>';
+    
+}
+
+    else if (total > randomNumber){
+    losses++;
+    $("#losses").html(losses);
+    $("#message").html("<h2>You Lose!<br><br> Click Here for New Game </h2>");
+    document.getElementById("footer").innerHTML = '<audio id= "audio" autoplay src="assets/javascript/traps.mp3"></audio>';
+    
+
+    
+}};
+
 }
 
