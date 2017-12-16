432f1575a566d56ba68b5a0c70dab311b3f4c613
Jennifer Campbell <jpagliarulo@Staff-14.local>
2017-12-16 00:03:41 -0500

    so close

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,6 +9,7 @@ $("#message").click(reset);
 
 function reset(){
     total = 0;
+    var playerScore = 0;
 
     $("#score").text("");
     $("#message").html("");
@@ -18,16 +19,49 @@ function reset(){
     var randomNumber = Math.floor((Math.random() * (120 - 19 + 1)) + 19);
     $("#random").html(randomNumber);
 
-    $(".gem").click(player);
     
+    $("#a").click(clicka);
+    $("#b").click(clickb);
+    $("#c").click(clickc);
+    $("#d").click(clickd);
+    
+    var a = Math.floor((Math.random() * (12 - 1 + 1)) + 1);
+    var b = Math.floor((Math.random() * (12 - 1 + 1)) + 1);
+    var c = Math.floor((Math.random() * (12 - 1 + 1)) + 1);
+    var d = Math.floor((Math.random() * (12 - 1 + 1)) + 1);
+
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    console.log(d);
+    console.log("playerScore = " + playerScore);
+    console.log("total = " + total);
+
+function clicka (){
+    playerScore = a;
+    player();
+}
+
+function clickb (){
+    playerScore = b;
+    player();
+}
+
+function clickc (){
+    playerScore = c;
+    player();
+}
+
+function clickd (){
+    playerScore = d;
+    player();
+}
+
 function player(){
-    var playerScore = Math.floor((Math.random() * (12 - 1 + 1)) + 1);
-
-        total = total + playerScore;
     
-    
+    total = total + playerScore;
     $("#score").text(total);
-
+    console.log("player total = " + total);
     check();
 }
 
@@ -36,7 +70,7 @@ function check(){
     wins++;
     $("#wins").html(wins);
     $("#message").html("<h2>You Win!<br><br> Click Here for New Game </h2>");
-    $document.getElementById("footer").innerHTML = '<audio id= "audio"  autoplay src="assets/javascript/sloth.mp3"></audio>';
+    document.getElementById("footer").innerHTML = '<audio id= "audio"  autoplay src="assets/javascript/sloth.mp3"></audio>';
     
 }
 
